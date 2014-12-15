9271107685c52747179f72460f05ed6c341311c6
joscha <zippelman@web.de>
2014-12-15 23:17:27 +0100

    first attempt to switch to expresses

--- controllers/intervals.js
+++ controllers/intervals.js
@@ -1,14 +1,12 @@
 var interval = require('../models/interval');
 
-exports.index = {
-    handler: function (request, reply) {
-        var data =
-            '<h1> Hi there! </h1>' +
-            '<p> Would you like to <a href="login">login</a> or <a href="register">register</a>? </p>';
-
-        reply(data);
-    }
+exports.index = function (req, res) {
+    var data =
+        '<h1> Hi there! </h1>' +
+        '<p> Would you like to <a href="login">login</a> or <a href="register">register</a>? </p>';
+    res.send(data);
 };
-function getWorkedTime(userid, date){
+
+function getWorkedTime(userid, date) {
 
 };
--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-var Hapi = require('hapi');
+var express = require('express');
+var bodyParser = require('body-parser');
+var intervals = require('./controllers/intervals');
 
-// Create a server with a host and port
-var server = Hapi.createServer(1337);
+var app = express();
 
-var Routes = require('./routes');  
 
-// Print some information about the incoming request for debugging purposes
-server.ext('onRequest', function (request, next) {  
-    console.log(request.path, request.query);
-    next();
+
+app.use(bodyParser.json());
+
+app.get('/', intervals.index);
+
+app.listen(3000, function(){
+    console.log('server started.');
 });
-
-server.route(Routes.endpoints);
-
-// Start the server
-//server.start(function() {
-//    console.log("The server has started on port: " + server.info.port);
-//});
-module.exports = server;
